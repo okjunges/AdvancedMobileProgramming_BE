bb56d1114f75ccea16b695b878505e53faa56c0d
okjunges <rladhkstn02@gmail.com>
2025-11-11 15:11:57 +0900

    복원

--- src/main/java/com/advancedMobileProgramming/AdvancedMobileProgrammingApplication.java
+++ src/main/java/com/advancedMobileProgramming/AdvancedMobileProgrammingApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class AdvancedMobileProgrammingApplication {
-	//dd
+
 	public static void main(String[] args) {
 		SpringApplication.run(AdvancedMobileProgrammingApplication.class, args);
 	}
